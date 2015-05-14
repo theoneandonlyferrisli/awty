968872eb8765a0b094d208a1698eb747d68fc420
Yuefeng Li <radek@uw.edu>
2015-05-14 16:35:52 -0700

    user input validation added

--- app/src/main/java/are_we_there_yet/radek/washington/edu/arewethereyet/MainActivity.java
+++ app/src/main/java/are_we_there_yet/radek/washington/edu/arewethereyet/MainActivity.java
@@ -2,9 +2,15 @@ package are_we_there_yet.radek.washington.edu.arewethereyet;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.regex.Pattern;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,12 +20,27 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText phoneNumber = (EditText) findViewById(R.id.phone_number);
-        EditText message = (EditText) findViewById(R.id.message);
-        
+        // Connect the activity to its controls.
+        final EditText phoneNumberBox = (EditText) findViewById(R.id.phone_number);
+        final EditText messageBox = (EditText) findViewById(R.id.message);
+        final EditText durationBox = (EditText) findViewById(R.id.duration);
+        final Button start = (Button) findViewById(R.id.start_and_stop);
 
-        String number = phoneNumber.getText().toString();
-        String msg = message.getText().toString();
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Pattern phoneNumberValidator = Pattern.compile("^[0-9]{10}$");
+                Pattern durationValidator = Pattern.compile(("^[1-9][0-9]*$"));
+                if (!phoneNumberBox.getText().toString().equals("") &&
+                    !messageBox.getText().toString().equals("") &&
+                    !durationBox.getText().toString().equals("") &&
+                    phoneNumberValidator.matcher(phoneNumberBox.getText().toString()).matches() &&
+                    durationValidator.matcher(durationBox.getText().toString()).matches()) {
+                    start.setText("Stop");
+                }
+            }
+        });
+
     }
 
     @Override
