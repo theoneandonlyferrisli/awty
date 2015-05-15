0bd67f78419046a54a81b840f79c99c0597bec0b
Yuefeng Li <radek@uw.edu>
2015-05-14 17:17:49 -0700

    project finished

--- app/src/main/java/are_we_there_yet/radek/washington/edu/arewethereyet/Receiver.java
+++ app/src/main/java/are_we_there_yet/radek/washington/edu/arewethereyet/Receiver.java
@@ -1,8 +1,19 @@
 package are_we_there_yet.radek.washington.edu.arewethereyet;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
 /**
  * Created by apple on 5/14/15.
  */
-public class Receiver {
+public class Receiver extends BroadcastReceiver {
 
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent != null) {
+            Toast.makeText(context, intent.getStringExtra("message"), Toast.LENGTH_LONG).show();
+        }
+    }
 }
